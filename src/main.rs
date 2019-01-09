6d2bbf5d31388fa51ab45916c9a2da89ec2ede61
Dgame <rswhite4@googlemail.com>
2019-01-09 12:47:56 +0100

    Adapt to changes of just-core

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
-use just_core::blueprint::Blueprint;
-use just_core::kernel::LocalPackage;
+use just_core::kernel::Kernel;
+use just_core::manifest::Manifest;
 use just_core::result::BoxedResult;
 use semver::VersionReq;
 use structopt::StructOpt;
@@ -13,69 +13,100 @@ struct Opt {
     pub version: Option<VersionReq>,
 }
 
-fn install(blueprint: &Blueprint, req: Option<VersionReq>) -> BoxedResult<()> {
-    use just_core::kernel::Kernel;
-    use just_download::download;
-    use just_extract::extract;
-    use just_fetch::Fetch;
-    use log::info;
+struct Install<'a> {
+    kernel: &'a mut Kernel,
+    manifest: &'a Manifest,
+    req: Option<VersionReq>,
+}
 
-    let mut kernel = Kernel::load();
-    let mut fetch = Fetch::new(&blueprint, &mut kernel.versions);
-    if fetch.needs_fetch() {
-        fetch.fetch_all_versions()?;
+impl<'a> Install<'a> {
+    fn new(kernel: &'a mut Kernel, manifest: &'a Manifest, req: Option<VersionReq>) -> Self {
+        Self {
+            kernel,
+            manifest,
+            req,
+        }
     }
 
-    let package = &blueprint.package;
-    if kernel.packages.is_installed(package, req.clone()) {
-        Ok(())
-    } else if let Some((version, path)) = kernel
-        .downloads
-        .get_download(&package.name, &req.clone().unwrap())
-    {
-        let local = LocalPackage {
-            package,
-            version,
-            path,
-        };
+    fn install(&mut self) -> BoxedResult<()> {
+        use just_core::kernel::LocalPackage;
+        use just_download::download;
+        use just_extract::extract;
+        use just_fetch::Fetch;
+        use log::info;
 
-        kernel.packages.add_package(local.package, local.version);
-        kernel.versions.add_version(local.package, local.version);
-        kernel.create_shims(&local)
-    } else {
-        let name = package.name.as_str();
+        let mut fetch = Fetch::new(&self.manifest, &mut self.kernel.versions);
+        if fetch.needs_fetch() {
+            fetch.fetch_all_versions()?;
+        }
 
-        info!("Downloading package {}...", name);
-        let info = download(blueprint, req)?;
-        info!("Extracting package {}...", name);
-        extract(&info.uncompressed_path, &info.compressed_path).and_then(|_| {
+        let package = &self.manifest.package;
+        if self.kernel.packages.is_installed(package, self.req.clone()) {
+            Ok(())
+        } else if let Some((version, path)) = self
+            .kernel
+            .downloads
+            .get_download(&package.name, &self.req.clone().unwrap())
+        {
             let local = LocalPackage {
                 package,
-                version: &info.version,
-                path: &info.uncompressed_path,
+                version,
+                path,
             };
 
-            kernel
-                .downloads
-                .add_download(&local, &kernel.path.downloads);
-            kernel.packages.add_package(local.package, local.version);
-            kernel.versions.add_version(local.package, local.version);
-            kernel.create_shims(&local)
-        })
+            self.kernel
+                .packages
+                .add_package(local.package, local.version);
+            self.kernel
+                .versions
+                .add_version(local.package, local.version);
+            self.kernel.create_shims(&local)
+        } else {
+            let name = package.name.as_str();
+
+            info!("Downloading package {}...", name);
+            let info = download(self.manifest, self.req.clone())?;
+            info!("Extracting package {}...", name);
+            extract(&info.uncompressed_path, &info.compressed_path).and_then(|_| {
+                let local = LocalPackage {
+                    package,
+                    version: &info.version,
+                    path: &info.uncompressed_path,
+                };
+
+                self.kernel
+                    .downloads
+                    .add_download(&local, &self.kernel.path.download_path);
+                self.kernel
+                    .packages
+                    .add_package(local.package, local.version);
+                self.kernel
+                    .versions
+                    .add_version(local.package, local.version);
+                self.kernel.create_shims(&local)
+            })
+        }
     }
 }
 
 fn main() {
-    use just_core::blueprint::Blueprints;
+    use just_core::manifest::ManifestFiles;
 
     let opt: Opt = Opt::from_args();
-    if let Some(pkg) = opt.package {
-        if let Some(blueprint) = Blueprints::scan().load(&pkg) {
-            let req = opt.version.or_else(|| Some(VersionReq::any()));
-            install(&blueprint, req)
-                .unwrap_or_else(|e| panic!("Could not install package {}: {:?}", pkg, e));
+    if let Some(pkg_name) = opt.package {
+        let mut kernel = Kernel::load();
+
+        if let Some(manifest) = ManifestFiles::scan(&kernel).load_manifest(&pkg_name) {
+            let mut install = Install::new(
+                &mut kernel,
+                &manifest,
+                opt.version.or_else(|| Some(VersionReq::any())),
+            );
+            install
+                .install()
+                .unwrap_or_else(|e| panic!("Could not install package {}: {:?}", pkg_name, e));
         } else {
-            println!("Package {:?} does not exists", pkg);
+            println!("Package {:?} does not exists", pkg_name);
         }
     }
 }
